42085becdf8f997bd4da58f4c34aaf954a9d83ba
Jhonata <jhonbonadio@gmail.com>
2024-11-13 11:06:00 -0300

    notificar cancelamento de agendamento

--- src/services/DeleteAgendamentoStoreService.ts
+++ src/services/DeleteAgendamentoStoreService.ts
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import { prismaClient } from '../database/prismaClient'
 
 interface IRequest {
@@ -19,6 +20,10 @@ class DeleteAgendamentoStoreService {
     if (!agendamento) {
       throw new Error('Agendamento não encontrado ou não pertence ao usuário.')
     }
+
+    const prestador = await prismaClient.prestador.findFirst({
+      where: { id: usuarioId, deleted: false },
+    })
 
     // Verificar se o agendamento foi validado
     const validacao = await prismaClient.validacaoAgendamento.findFirst({
@@ -43,6 +48,43 @@ class DeleteAgendamentoStoreService {
         ativo: false,
       },
     })
+
+    async function sendNotification() {
+      await axios
+        .post(
+          'https://api.onesignal.com/notifications',
+          {
+            app_id: process.env.ONESIGNAL_APP_ID!,
+            name: 'Cancelamento',
+            target_channel: 'push',
+            headings: {
+              en: 'Seu agendamento foi cancelado',
+            },
+            include_aliases: { external_id: [agendamento?.usuarioId] },
+            contents: {
+              en:
+                'Seu agendamento com ' +
+                prestador?.razaoSocial +
+                ' foi cancelado. Entre no app para agendar novamente!',
+            },
+          },
+          {
+            headers: {
+              Authorization: process.env.ONESIGNAL_API_KEY!,
+            },
+          },
+        )
+        .then((data) => {
+          console.log(data)
+        })
+        .catch(({ response }) => {
+          console.log(response)
+        })
+    }
+
+    if (deleteAgendamento) {
+      sendNotification()
+    }
 
     return deleteAgendamento
   }
